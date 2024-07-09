915c4c61f04fe22ac02317b6966010087bbabaa3
Dilsoz21 <abduxolikova05@gmail.com>
2024-07-09 15:58:44 +0500

    first commit

---
+++ person/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from rest_framework.response import Response
+
+from .models import Person
+
+
+class PoetSerializers(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    class Meta:
+        model = Person
+        fields = "__all__"
+
